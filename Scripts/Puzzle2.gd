f5a642227c37d792bd9a95f8548fd93b66e861d7
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-02-04 11:01:30 +0100

    {Progress} Auto generation of tiles
    - 3/4 completed

--- Scripts/Puzzle2.gd
+++ Scripts/Puzzle2.gd
@@ -21,18 +21,19 @@ func _ready():
 
 func createPath():
 	randomize()
+	# create the first tile
 	var first_tile = int(rand_range(0, 3))
 	var transpose = first_tile == 0 # first_tile was a straight line
-	self.set_cellv(Vector2(0, 0), first_tile, false, false, transpose)
-	
+	self.set_cellv(Vector2.ZERO, first_tile, false, false, transpose)
+	# loop until we 
 #	createTile(Vector2.ZERO)
 
 
-func createTile(from):
+func createTile(from) -> void:
 #	tile 0 = straight bar 
 #	tile 1 = corner
 #	tile 2 = t shape
-	if from == size - 1:
+	if from == Vector2(size.x-1, size.y-1):
 		return
 	var previous = tilemap[from.x][from.y]
 	var positions = []
@@ -42,31 +43,66 @@ func createTile(from):
 		1: # corner
 			positions = posFrom1(from)
 		2: # t
+			positions = posFrom2(from)
+	if positions == []:
+		return
+	for pos in positions:
+		var tiles = Array(range(3))
+		tiles.shuffle()
+		for tile in tiles:
+			createTile(pos)
+
+
+func get_rotations(from: Vector2, to: Vector2) -> Array:
+	var rotations: Array
+	match get_cellv(to):
+		0:
+			var transpose = false
+			match get_cellv(from):
+				0:
+					rotations = [[false, false, transpose]]
+				1:
+					pass
+				2:
+					pass
+		1:
 			pass
+		2:
+			pass
+	return rotations
 
 
-
-func posFrom0(from):
+func posFrom0(from: Vector2) -> Array:
 	var positions = []
 	if is_cell_transposed(from.x, from.y):
 		# vertical line
-		if from.y != 0: # top bound
-			positions.append(Vector2(from.x, from.y-1))
-		if from.y != size.y - 1: # bot bound
-			positions.appen(Vector2(from.x, from.y+1))
+		var pos1 = Vector2(from.x, from.y-1)
+		var pos2 = Vector2(from.x, from.y+1)
+		if from.y != 0 and get_cellv(pos1) == -1: # top bound
+			positions.append(pos1)
+		if from.y != size.y - 1 and get_cellv(pos2) == -1: # bot bound
+			positions.append(pos2)
 	else:
 		# horizontal line
-		if from.x != 0: # left bound
-			positions.append(Vector2(from.x-1, from.y))
-		if from.x != size.x - 1: # right bound:
-			positions.append(Vector2(from.x+1, from.y))
+		var pos1 = Vector2(from.x-1, from.y)
+		var pos2 = Vector2(from.x+1, from.y)
+		if from.x != 0 and get_cellv(pos1): # left bound
+			positions.append(pos1)
+		if from.x != size.x - 1 and get_cellv(pos2): # right bound:
+			positions.append(pos2)
 	return positions
 
-func posFrom1(from):
+
+func posFrom1(from: Vector2) -> Array:
 	var positions = []
 	if is_cell_x_flipped(from.x, from.y): # -90°
 		positions.append()
+	return positions
 
+
+func posFrom2(from: Vector2) -> Array:
+	var positions = []
+	return positions
 
 func _input(event):
 	if not(event is InputEventMouseButton) or \
@@ -74,4 +110,21 @@ func _input(event):
 	not (event.pressed):
 		return
 	var pos = world_to_map(event.position)
-	print(pos, ": ", get_cell(pos.x, pos.y))
+	rotateTile(pos)
+
+func rotateTile(pos: Vector2):
+	var x = pos.x
+	var y = pos.y
+	match get_cellv(pos):
+		0:
+			set_cellv(pos, 0, false, false, not is_cell_transposed(x, y))
+		1:
+			var flip_h = is_cell_transposed(x, y)
+			var transpose = is_cell_y_flipped(x, y)
+			var flip_v = not flip_h and not transpose and not is_cell_x_flipped(x, y)
+			set_cellv(pos, 1, flip_h, flip_v, transpose)
+		2:
+			var flip_v = is_cell_x_flipped(x, y) and is_cell_transposed(x, y)
+			var flip_h = not(is_cell_x_flipped(x, y) or is_cell_transposed(x, y) or is_cell_y_flipped(x, y))
+			var transpose = is_cell_y_flipped(x, y) or flip_h
+			set_cellv(pos, 2, flip_h, flip_v, transpose)
